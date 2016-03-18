a859cfa0833512ede9f22ec1e2d642908fbb549d
Chinseok Lee <allieuslee@gmail.com>
2016-03-18 15:54:29 +0900

    댓글달기 기능 추가

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Post(models.Model):
@@ -6,7 +7,13 @@ class Post(models.Model):
     content = models.TextField()
     photo = models.ImageField()
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post)
     memssage = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.post_id])
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView, CreateView
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post, Comment
+from .forms import CommentForm
 
 
 # def post_list(request):
@@ -9,3 +10,15 @@ from .models import Post, Comment
 post_list = ListView.as_view(model=Post)
 
 post_detail = DetailView.as_view(model=Post)
+
+class CommentCreateView(CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = get_object_or_404(Post, pk=self.kwargs['post_pk'])
+        comment.save()
+        return super(CommentCreateView, self).form_valid(form)
+
+comment_new = CommentCreateView.as_view()
